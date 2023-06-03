2379a18165b6c611cd7e7c1c498e02d3030f8b50
brandonT2002 <3060571900304@ingenieria.usac.edu.gt>
2023-06-03 11:13:07 -0600

    se genera el codigo dot de la cola

--- Cola/main.go
+++ Cola/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"strconv"
 )
 
@@ -11,37 +12,84 @@ type Nodo struct {
 }
 
 type Queue struct {
-	primero *Nodo
-	ultimo  *Nodo
+	primero  *Nodo
+	ultimo   *Nodo
+	longitud int
 }
 
 func (nodo *Queue) insertar(valor int) {
 	if nodo.primero != nil {
 		nodo.ultimo.siguiente = &Nodo{valor: valor}
 		nodo.ultimo = nodo.ultimo.siguiente
+		nodo.longitud++
 		return
 	}
 	nodo.primero = &Nodo{valor: valor}
 	nodo.ultimo = nodo.primero
+	nodo.longitud++
 }
 
 func (nodo *Queue) pop() interface{} {
 	if nodo.primero != nil {
 		temporal := nodo.primero.valor
 		nodo.primero = nodo.primero.siguiente
+		nodo.longitud--
 		return temporal
 	}
 	return nil
 }
 
-func (nodo *Queue) recorrer() string {
+func (nodo *Queue) dot() string {
+	dot := "digraph G {\n"
+	dot += "rankdir=LR;\n"
+	dot += "node[shape=\"box\"];\n"
+
 	actual := nodo.primero
-	queue := ""
+	long := 1
 	for actual != nil {
-		queue += " -> " + strconv.Itoa(actual.valor)
+		if long < nodo.longitud {
+			dot += strconv.Itoa(actual.valor) + " -> "
+			long++
+		} else {
+			dot += strconv.Itoa(actual.valor)
+		}
+		if actual.siguiente == nil {
+			dot += ";\n"
+		}
 		actual = actual.siguiente
 	}
-	return queue
+	dot += "}\n"
+
+	return dot
+}
+
+func generarGrafo(dot string) {
+	nombreArchivo := "grafo.dot"
+	nuevoContenido := dot
+
+	// Eliminar el archivo existente
+	err := os.Remove(nombreArchivo)
+	if err != nil && !os.IsNotExist(err) {
+		fmt.Println("Error al eliminar el archivo:", err)
+		return
+	}
+
+	// Crear un nuevo archivo
+	file, err := os.Create(nombreArchivo)
+	if err != nil {
+		fmt.Println("Error al crear el archivo:", err)
+		return
+	}
+	defer file.Close()
+
+	// Escribir el nuevo contenido en el archivo
+	_, err = file.WriteString(nuevoContenido)
+	if err != nil {
+		fmt.Println("Error al escribir en el archivo:", err)
+		return
+	}
+
+	fmt.Println("Se ha escrito el nuevo contenido en el archivo.")
 }
 
 func main() {
@@ -50,9 +98,5 @@ func main() {
 	q1.insertar(2)
 	q1.insertar(3)
 	q1.insertar(4)
-	fmt.Println(q1.recorrer())
-
-	q1.pop()
-	q1.pop()
-	fmt.Println(q1.recorrer())
+	generarGrafo(q1.dot())
 }
