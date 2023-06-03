d0963a08138e4b9cfed7bd3f49b856abe88fa189
brandonT2002 <3060571900304@ingenieria.usac.edu.gt>
2023-06-03 10:51:00 -0600

    se genera el codigo dot de la lista doble

--- ListaDoble/main.go
+++ ListaDoble/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"strconv"
 )
 
@@ -12,8 +13,9 @@ type Nodo struct {
 }
 
 type ListaDoble struct {
-	primero *Nodo
-	ultimo  *Nodo
+	primero  *Nodo
+	ultimo   *Nodo
+	longitud int
 }
 
 func (nodo *ListaDoble) insertar(valor int) {
@@ -21,30 +23,83 @@ func (nodo *ListaDoble) insertar(valor int) {
 		nodo.ultimo.siguiente = &Nodo{valor: valor}
 		nodo.ultimo.siguiente.anterior = nodo.ultimo
 		nodo.ultimo = nodo.ultimo.siguiente
+		nodo.longitud += 1
 		return
 	}
 	nodo.primero = &Nodo{valor: valor}
 	nodo.ultimo = nodo.primero
+	nodo.longitud += 1
 }
 
-func (nodo *ListaDoble) recorrer1() string {
+func (nodo *ListaDoble) dot() string {
+	dot := "digraph G {\n"
+	dot += "rankdir=LR;\n"
+	dot += "node[shape=\"box\"];\n"
+
+	dot += "nodo1 [label=\"null\"]\n"
+	dot += "nodo2 [label=\"null\"]\n"
+
 	actual := nodo.primero
-	lista := ""
-	for actual != nil {
-		lista += " -> " + strconv.Itoa(actual.valor)
+	contador := 0
+	for contador < 2 && actual != nil {
+		dot += strconv.Itoa(actual.valor) + " -> "
+		if actual.siguiente == nil {
+			dot += "nodo1;\n"
+		}
 		actual = actual.siguiente
 	}
-	return lista
+
+	actual = nodo.primero
+	dot += "nodo2"
+	for actual != nil {
+		dot += " -> " + strconv.Itoa(actual.valor)
+		if actual.siguiente == nil {
+			dot += "[dir=\"back\"] ;\n"
+		}
+		actual = actual.siguiente
+	}
+	dot += "}"
+
+	return dot
 }
 
-func (nodo *ListaDoble) recorrer2() string {
-	actual := nodo.ultimo
-	lista := ""
-	for actual != nil {
-		lista += " -> " + strconv.Itoa(actual.valor)
-		actual = actual.anterior
+// func (nodo *ListaDoble) recorrer2() string {
+// 	actual := nodo.ultimo
+// 	lista := ""
+// 	for actual != nil {
+// 		lista += " -> " + strconv.Itoa(actual.valor)
+// 		actual = actual.anterior
+// 	}
+// 	return lista
+// }
+
+func generarGrafo(dot string) {
+	nombreArchivo := "grafo.dot"
+	nuevoContenido := dot
+
+	// Eliminar el archivo existente
+	err := os.Remove(nombreArchivo)
+	if err != nil && !os.IsNotExist(err) {
+		fmt.Println("Error al eliminar el archivo:", err)
+		return
 	}
-	return lista
+
+	// Crear un nuevo archivo
+	file, err := os.Create(nombreArchivo)
+	if err != nil {
+		fmt.Println("Error al crear el archivo:", err)
+		return
+	}
+	defer file.Close()
+
+	// Escribir el nuevo contenido en el archivo
+	_, err = file.WriteString(nuevoContenido)
+	if err != nil {
+		fmt.Println("Error al escribir en el archivo:", err)
+		return
+	}
+
+	fmt.Println("Se ha escrito el nuevo contenido en el archivo.")
 }
 
 func main() {
@@ -53,6 +108,5 @@ func main() {
 	l1.insertar(2)
 	l1.insertar(3)
 	l1.insertar(4)
-	fmt.Println(l1.recorrer1())
-	fmt.Println(l1.recorrer2())
+	generarGrafo(l1.dot())
 }
