3f4c51389ae817b438e04cf457fa2169bdcb273b
brandonT2002 <brandonteja02@gmail.com>
2023-07-19 10:22:25 -0600

    lista simple

--- Java/ListaSimple/Main.java
+++ Java/ListaSimple/Main.java
@@ -13,24 +13,20 @@ class Nodo {
 class ListaSimple {
     Nodo primero;
     Nodo ultimo;
-    int longitud;
 
     public ListaSimple() {
         primero = null;
         ultimo = null;
-        longitud = 0;
     }
 
     public void Insertar(int valor) {
         if (this.primero != null) {
             this.ultimo.siguiente = new Nodo(valor);
             this.ultimo = this.ultimo.siguiente;
-            this.longitud += 1;
             return;
         }
         this.primero = new Nodo(valor);
         this.ultimo = this.primero;
-        this.longitud += 1;
     }
 
     public String recorrer () {
@@ -50,9 +46,9 @@ public class Main {
         ListaSimple l1 = new ListaSimple();
         l1.Insertar(1);
         l1.Insertar(2);
-        l1.Insertar(2);
         l1.Insertar(3);
         l1.Insertar(4);
+        l1.Insertar(5);
         System.out.println(l1.recorrer());
     }
 
